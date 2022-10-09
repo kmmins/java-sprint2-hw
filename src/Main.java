52a3c131b779d8a9c8592945f44ca71e035e6400
kmmins <kmmins@yandex.ru>
2022-10-09 13:46:16 +0300

    homework is ready 3.

--- src/Main.java
+++ src/Main.java
@@ -4,73 +4,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Программа помошник Бухгалтера Парка Развлечений");
+        ReportsService reportsService = new ReportsService();
 
-        MonthlyReport[] monthReps = new MonthlyReport[3];
-        YearlyReport yearRep = null;
 
-        while (true) {
+        imOutOfHere: while (true) {
             printMenu();
-            int command = scanner.nextInt();
-            if (command == 1) {
-                for (int i = 0; i < monthReps.length; i++) {
-                    monthReps[i] = new MonthlyReport("resources/m.20210" + (i + 1) + ".csv", (i+1));
-                }
-                System.out.println("Месячные отчёты считаны");
-            }
-            else if (command == 2) {
-                yearRep = new YearlyReport("resources/y.2021.csv", 2021);
-                System.out.println("Годовой отчёт считан");
-            }
-            else if (command == 3) {
-                System.out.println("Сверка отчётов...");
-                boolean containsNull = false;
-                for (MonthlyReport element : monthReps) {
-                    if (element == null) {
-                        containsNull = true;
-                        break;
-                    }
-                }
-                if (containsNull || yearRep == null) {
-                    System.out.println("Ошибка!\r\nДля сверки сначала необходимо считать все месячные и годовые отчеты!\r\nНажмите кнопочки [1] и [2]");
-                } else {
-                    boolean sverkaUspeh = true;
-                    for (int i = 0; i < monthReps.length; i++) {
-                        monthReps[i].sverkaSumMonth();
-                        if (monthReps[i].sumExpensMonth != yearRep.data[i].expenses ||
-                            monthReps[i].sumIncomeMonth != yearRep.data[i].income) {
-                            sverkaUspeh = false;
-                            System.out.println("Обнаружена ошибка! Месяц в котором ошибка: " + (i + 1));
-                        }
-                    }
-                    if (sverkaUspeh) System.out.println("Сверка произведена успешно!");
-                }
-            }
-            else if (command == 4) {
-                System.out.println("Информация о всех месячных отчётах: ");
-
-                for (MonthlyReport element : monthReps) {
-                    if (element != null) {
-                        element.infoMonth();
-                    } else {
-                        System.out.println("Ошибка!\r\nДля вывода информации о месечных отчетах сначала небходимо считать все месячные!\r\nНажмите кнопочку [1]");
-                        break;
-                    }
-                }
-            }
-            else if (command == 5) {
-                System.out.println("Информация о годовом отчёте: ");
-                if (yearRep != null) {
-                    yearRep.infoYear();
-                } else {
-                    System.out.println("Ошибка!\r\nДля вывода информации о годовом отчете сначала небходимо считать все годовые отчеты!\r\nНажмите кнопочку [2]");
-                }
-            }
-            else if (command == 6) {
-                System.out.println("Выход");
-                break;
-            }
-            else {
-                System.out.println("Извините, такой команды пока нет");
+            int commandSwitch = scanner.nextInt();
+            switch (commandSwitch) {
+                case 1:
+                    reportsService.readMR();
+                    System.out.println("Месячные отчёты считаны");
+                    break;
+                case 2:
+                    reportsService.readYR();
+                    System.out.println("Годовой отчёт считан");
+                    break;
+                case 3:
+                    System.out.println("Сверка отчётов...");
+                    reportsService.compareMY();
+                    break;
+                case 4:
+                    System.out.println("Информация о всех месячных отчётах: ");
+                    reportsService.showInfoM();
+                    break;
+                case 5:
+                    System.out.println("Информация о годовом отчёте: ");
+                    reportsService.showInfoY();
+                    break;
+                case 6:
+                    System.out.println("Выход...");
+                    break imOutOfHere;
+                default: System.out.println("Извините, такой команды пока нет");
             }
         }
     }
---
+++ src/ReportsService.java
@@ -0,0 +1,61 @@
+public class ReportsService {
+
+    MonthlyReport[] monthReps = new MonthlyReport[3];
+    YearlyReport yearRep = null;
+
+    public void readMR() {
+
+        for (int i = 0; i < monthReps.length; i++) {
+            monthReps[i] = new MonthlyReport(i + 1);
+            monthReps[i].loadFromFileContents("resources/m.20210" + (i + 1) + ".csv");
+        }
+    }
+    public void readYR() {
+
+        yearRep = new YearlyReport(2021);
+        yearRep.loadFromFileContents("resources/y.2021.csv");
+    }
+    public void compareMY () {
+
+        boolean containsNull = false;
+        for (MonthlyReport element : monthReps) {
+            if (element == null) {
+                containsNull = true;
+                break;
+            }
+        }
+        if (containsNull || yearRep == null) {
+            System.out.println("Ошибка!\r\nДля сверки сначала необходимо считать все месячные и годовые отчеты!\r\nНажмите кнопочки [1] и [2]");
+        } else {
+            boolean sverkaUspeh = true;
+            for (int i = 0; i < monthReps.length; i++) {
+                monthReps[i].sverkaSumMonth();
+                if (monthReps[i].sumExpensMonth != yearRep.data[i].expenses ||
+                        monthReps[i].sumIncomeMonth != yearRep.data[i].income) {
+                    sverkaUspeh = false;
+                    System.out.println("Обнаружена ошибка! Месяц в котором ошибка: " + (i + 1));
+                }
+            }
+            if (sverkaUspeh) System.out.println("Сверка произведена успешно!");
+        }
+    }
+    public void showInfoM () {
+
+        for (MonthlyReport element : monthReps) {
+            if (element != null) {
+                element.infoMonth();
+            } else {
+                System.out.println("Ошибка!\r\nДля вывода информации о месечных отчетах сначала небходимо считать все месячные!\r\nНажмите кнопочку [1]");
+                break;
+            }
+        }
+    }
+    public void showInfoY (){
+
+        if (yearRep != null) {
+            yearRep.infoYear();
+        } else {
+            System.out.println("Ошибка!\r\nДля вывода информации о годовом отчете сначала небходимо считать все годовые отчеты!\r\nНажмите кнопочку [2]");
+        }
+    }
+}
